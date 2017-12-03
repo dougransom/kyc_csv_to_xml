06420dcea5c55f45e8c81687dc312fea1c64dc60
Doug Ransom <doug.ransom@gmail.com>
2017-12-03 07:14:30 -0800

    ready to release

--- build_kyc_csv_to_xml_executable.ps1
+++ build_kyc_csv_to_xml_executable.ps1
@@ -1,2 +1,4 @@
 #compiles our python script to an exectuable
+#then builds a zip file with readme, the exectuable, and the python source.
 pyinstaller --noconfirm --onefile kyc_csv_to_adobe_xml.py
+Compress-Archive -Force -Path .\dist\kyc_csv_to_adobe_xml.exe, .\readme.txt, .\kyc_csv_to_adobe_xml.py -DestinationPath dist\kyc_csv_to_xml.zip
